aaddcaf1f9e8314c3ff10e1dc04a80e547bb0a2d
Subhasish <greendoor3000@gmail.com>
2026-07-20 11:23:47 +0530

    Add Heze block containers and complete the ssz_static spec tests
    - Add BeaconBlock, BeaconBlockBody, and SignedBeaconBlock for heze. BeaconBlockBody holds the
    heze SignedExecutionPayloadBid. BeaconBlock carries it through its body` and SignedBeaconBlock
    through its `message`.
    -  Extend heze/spec_tests covering the remaining ssz_static types.

--- types/src/heze/containers.rs
+++ types/src/heze/containers.rs
@@ -1,12 +1,20 @@
 use bls::SignatureBytes;
 use serde::{Deserialize, Serialize};
-use ssz::{BitVector, ProgressiveList, Ssz};
+use ssz::{BitVector, Hc, ProgressiveList, Ssz};
 
 use crate::{
+    altair::containers::SyncAggregate,
     bellatrix::primitives::Gas,
+    capella::containers::SignedBlsToExecutionChange,
     deneb::primitives::KzgCommitment,
-    gloas::primitives::{BuilderIndex, Transaction},
-    phase0::primitives::{ExecutionAddress, ExecutionBlockHash, Gwei, H256, Slot, ValidatorIndex},
+    gloas::{
+        containers::{Attestation, AttesterSlashing, ExecutionRequests, PayloadAttestation},
+        primitives::{BuilderIndex, Transaction},
+    },
+    phase0::{
+        containers::{Deposit, Eth1Data, ProposerSlashing, SignedVoluntaryExit},
+        primitives::{ExecutionAddress, ExecutionBlockHash, Gwei, H256, Slot, ValidatorIndex},
+    },
     preset::Preset,
 };
 
@@ -56,5 +64,43 @@ pub struct ExecutionPayloadBid<P: Preset> {
 #[serde(bound = "", deny_unknown_fields)]
 pub struct SignedExecutionPayloadBid<P: Preset> {
     pub message: ExecutionPayloadBid<P>,
+    pub signature: SignatureBytes,
+}
+
+#[derive(Clone, PartialEq, Eq, Debug, Default, Deserialize, Serialize, Ssz)]
+#[serde(bound = "", deny_unknown_fields)]
+pub struct BeaconBlock<P: Preset> {
+    #[serde(with = "serde_utils::string_or_native")]
+    pub slot: Slot,
+    #[serde(with = "serde_utils::string_or_native")]
+    pub proposer_index: ValidatorIndex,
+    pub parent_root: H256,
+    pub state_root: H256,
+    pub body: BeaconBlockBody<P>,
+}
+
+#[derive(Clone, PartialEq, Eq, Debug, Default, Deserialize, Serialize, Ssz)]
+#[serde(bound = "", deny_unknown_fields)]
+#[ssz(stable(active = [1; 13]))]
+pub struct BeaconBlockBody<P: Preset> {
+    pub randao_reveal: SignatureBytes,
+    pub eth1_data: Eth1Data,
+    pub graffiti: H256,
+    pub proposer_slashings: ProgressiveList<ProposerSlashing>,
+    pub attester_slashings: ProgressiveList<AttesterSlashing<P>>,
+    pub attestations: ProgressiveList<Attestation<P>>,
+    pub deposits: ProgressiveList<Deposit>,
+    pub voluntary_exits: ProgressiveList<SignedVoluntaryExit>,
+    pub sync_aggregate: SyncAggregate<P>,
+    pub bls_to_execution_changes: ProgressiveList<SignedBlsToExecutionChange>,
+    pub signed_execution_payload_bid: SignedExecutionPayloadBid<P>,
+    pub payload_attestations: ProgressiveList<PayloadAttestation<P>>,
+    pub parent_execution_requests: ExecutionRequests<P>,
+}
+
+#[derive(Clone, PartialEq, Eq, Debug, Deserialize, Serialize, Ssz)]
+#[serde(bound = "", deny_unknown_fields)]
+pub struct SignedBeaconBlock<P: Preset> {
+    pub message: Hc<BeaconBlock<P>>,
     pub signature: SignatureBytes,
 }
--- types/src/heze/spec_tests.rs
+++ types/src/heze/spec_tests.rs
@@ -7,10 +7,41 @@ use crate::{
 };
 
 mod tested_types {
-    pub use crate::heze::{
-        beacon_state::BeaconState,
-        containers::{
-            ExecutionPayloadBid, InclusionList, SignedExecutionPayloadBid, SignedInclusionList,
+    pub use crate::{
+        altair::containers::{
+            ContributionAndProof, SignedContributionAndProof, SyncAggregate,
+            SyncAggregatorSelectionData, SyncCommittee, SyncCommitteeContribution,
+            SyncCommitteeMessage,
+        },
+        bellatrix::containers::PowBlock,
+        capella::containers::{
+            BlsToExecutionChange, HistoricalSummary, SignedBlsToExecutionChange, Withdrawal,
+        },
+        electra::containers::{
+            ConsolidationRequest, DepositRequest, PendingConsolidation, PendingDeposit,
+            PendingPartialWithdrawal, SingleAttestation, WithdrawalRequest,
+        },
+        fulu::containers::{DataColumnsByRootIdentifier, MatrixEntry},
+        gloas::containers::{
+            AggregateAndProof, Attestation, AttesterSlashing, Builder, BuilderDepositRequest,
+            BuilderExitRequest, BuilderPendingPayment, BuilderPendingWithdrawal, DataColumnSidecar,
+            ExecutionPayload, ExecutionPayloadEnvelope, ExecutionRequests, IndexedAttestation,
+            IndexedPayloadAttestation, LightClientBootstrap, LightClientFinalityUpdate,
+            LightClientHeader, LightClientOptimisticUpdate, LightClientUpdate, PayloadAttestation,
+            PayloadAttestationData, PayloadAttestationMessage, ProposerPreferences,
+            SignedAggregateAndProof, SignedExecutionPayloadEnvelope, SignedProposerPreferences,
+        },
+        heze::{
+            beacon_state::BeaconState,
+            containers::{
+                BeaconBlock, BeaconBlockBody, ExecutionPayloadBid, InclusionList,
+                SignedBeaconBlock, SignedExecutionPayloadBid, SignedInclusionList,
+            },
+        },
+        phase0::containers::{
+            AttestationData, BeaconBlockHeader, Checkpoint, Deposit, DepositData, DepositMessage,
+            Eth1Data, Fork, ForkData, ProposerSlashing, SignedBeaconBlockHeader,
+            SignedVoluntaryExit, SigningData, Validator, VoluntaryExit,
         },
     };
 }
@@ -38,16 +69,158 @@ macro_rules! tests_for_type {
     };
 }
 
+// We do not run `consensus-spec-tests/tests/*/heze/ssz_static/Eth1Block/*/*`.
+// `Eth1Block` as defined in `consensus-specs` is meant as an example (an "abstract object").
+// For whatever reason there are tests for it anyway.
+
 tests_for_type! {
-    InclusionList<_>,
-    "consensus-spec-tests/tests/mainnet/heze/ssz_static/InclusionList/*/*",
-    "consensus-spec-tests/tests/minimal/heze/ssz_static/InclusionList/*/*",
+    AggregateAndProof<_>,
+    "consensus-spec-tests/tests/mainnet/heze/ssz_static/AggregateAndProof/*/*",
+    "consensus-spec-tests/tests/minimal/heze/ssz_static/AggregateAndProof/*/*",
 }
 
 tests_for_type! {
-    SignedInclusionList<_>,
-    "consensus-spec-tests/tests/mainnet/heze/ssz_static/SignedInclusionList/*/*",
-    "consensus-spec-tests/tests/minimal/heze/ssz_static/SignedInclusionList/*/*",
+    Attestation<_>,
+    "consensus-spec-tests/tests/mainnet/heze/ssz_static/Attestation/*/*",
+    "consensus-spec-tests/tests/minimal/heze/ssz_static/Attestation/*/*",
+}
+
+tests_for_type! {
+    AttestationData,
+    "consensus-spec-tests/tests/mainnet/heze/ssz_static/AttestationData/*/*",
+    "consensus-spec-tests/tests/minimal/heze/ssz_static/AttestationData/*/*",
+}
+
+tests_for_type! {
+    AttesterSlashing<_>,
+    "consensus-spec-tests/tests/mainnet/heze/ssz_static/AttesterSlashing/*/*",
+    "consensus-spec-tests/tests/minimal/heze/ssz_static/AttesterSlashing/*/*",
+}
+
+tests_for_type! {
+    BeaconBlock<_>,
+    "consensus-spec-tests/tests/mainnet/heze/ssz_static/BeaconBlock/*/*",
+    "consensus-spec-tests/tests/minimal/heze/ssz_static/BeaconBlock/*/*",
+}
+
+tests_for_type! {
+    BeaconBlockBody<_>,
+    "consensus-spec-tests/tests/mainnet/heze/ssz_static/BeaconBlockBody/*/*",
+    "consensus-spec-tests/tests/minimal/heze/ssz_static/BeaconBlockBody/*/*",
+}
+
+tests_for_type! {
+    BeaconBlockHeader,
+    "consensus-spec-tests/tests/mainnet/heze/ssz_static/BeaconBlockHeader/*/*",
+    "consensus-spec-tests/tests/minimal/heze/ssz_static/BeaconBlockHeader/*/*",
+}
+
+tests_for_type! {
+    BeaconState<_>,
+    "consensus-spec-tests/tests/mainnet/heze/ssz_static/BeaconState/*/*",
+    "consensus-spec-tests/tests/minimal/heze/ssz_static/BeaconState/*/*",
+}
+
+tests_for_type! {
+    BlsToExecutionChange,
+    "consensus-spec-tests/tests/mainnet/heze/ssz_static/BLSToExecutionChange/*/*",
+    "consensus-spec-tests/tests/minimal/heze/ssz_static/BLSToExecutionChange/*/*",
+}
+
+tests_for_type! {
+    Builder,
+    "consensus-spec-tests/tests/mainnet/heze/ssz_static/Builder/*/*",
+    "consensus-spec-tests/tests/minimal/heze/ssz_static/Builder/*/*",
+}
+
+tests_for_type! {
+    BuilderDepositRequest,
+    "consensus-spec-tests/tests/mainnet/heze/ssz_static/BuilderDepositRequest/*/*",
+    "consensus-spec-tests/tests/minimal/heze/ssz_static/BuilderDepositRequest/*/*",
+}
+
+tests_for_type! {
+    BuilderExitRequest,
+    "consensus-spec-tests/tests/mainnet/heze/ssz_static/BuilderExitRequest/*/*",
+    "consensus-spec-tests/tests/minimal/heze/ssz_static/BuilderExitRequest/*/*",
+}
+
+tests_for_type! {
+    BuilderPendingPayment,
+    "consensus-spec-tests/tests/mainnet/heze/ssz_static/BuilderPendingPayment/*/*",
+    "consensus-spec-tests/tests/minimal/heze/ssz_static/BuilderPendingPayment/*/*",
+}
+
+tests_for_type! {
+    BuilderPendingWithdrawal,
+    "consensus-spec-tests/tests/mainnet/heze/ssz_static/BuilderPendingWithdrawal/*/*",
+    "consensus-spec-tests/tests/minimal/heze/ssz_static/BuilderPendingWithdrawal/*/*",
+}
+
+tests_for_type! {
+    Checkpoint,
+    "consensus-spec-tests/tests/mainnet/heze/ssz_static/Checkpoint/*/*",
+    "consensus-spec-tests/tests/minimal/heze/ssz_static/Checkpoint/*/*",
+}
+
+tests_for_type! {
+    ConsolidationRequest,
+    "consensus-spec-tests/tests/mainnet/heze/ssz_static/ConsolidationRequest/*/*",
+    "consensus-spec-tests/tests/minimal/heze/ssz_static/ConsolidationRequest/*/*",
+}
+
+tests_for_type! {
+    ContributionAndProof<_>,
+    "consensus-spec-tests/tests/mainnet/heze/ssz_static/ContributionAndProof/*/*",
+    "consensus-spec-tests/tests/minimal/heze/ssz_static/ContributionAndProof/*/*",
+}
+
+tests_for_type! {
+    DataColumnSidecar<_>,
+    "consensus-spec-tests/tests/mainnet/heze/ssz_static/DataColumnSidecar/*/*",
+    "consensus-spec-tests/tests/minimal/heze/ssz_static/DataColumnSidecar/*/*",
+}
+
+tests_for_type! {
+    DataColumnsByRootIdentifier<_>,
+    "consensus-spec-tests/tests/mainnet/heze/ssz_static/DataColumnsByRootIdentifier/*/*",
+    "consensus-spec-tests/tests/minimal/heze/ssz_static/DataColumnsByRootIdentifier/*/*",
+}
+
+tests_for_type! {
+    Deposit,
+    "consensus-spec-tests/tests/mainnet/heze/ssz_static/Deposit/*/*",
+    "consensus-spec-tests/tests/minimal/heze/ssz_static/Deposit/*/*",
+}
+
+tests_for_type! {
+    DepositData,
+    "consensus-spec-tests/tests/mainnet/heze/ssz_static/DepositData/*/*",
+    "consensus-spec-tests/tests/minimal/heze/ssz_static/DepositData/*/*",
+}
+
+tests_for_type! {
+    DepositMessage,
+    "consensus-spec-tests/tests/mainnet/heze/ssz_static/DepositMessage/*/*",
+    "consensus-spec-tests/tests/minimal/heze/ssz_static/DepositMessage/*/*",
+}
+
+tests_for_type! {
+    DepositRequest,
+    "consensus-spec-tests/tests/mainnet/heze/ssz_static/DepositRequest/*/*",
+    "consensus-spec-tests/tests/minimal/heze/ssz_static/DepositRequest/*/*",
+}
+
+tests_for_type! {
+    Eth1Data,
+    "consensus-spec-tests/tests/mainnet/heze/ssz_static/Eth1Data/*/*",
+    "consensus-spec-tests/tests/minimal/heze/ssz_static/Eth1Data/*/*",
+}
+
+tests_for_type! {
+    ExecutionPayload<_>,
+    "consensus-spec-tests/tests/mainnet/heze/ssz_static/ExecutionPayload/*/*",
+    "consensus-spec-tests/tests/minimal/heze/ssz_static/ExecutionPayload/*/*",
 }
 
 tests_for_type! {
@@ -57,13 +230,265 @@ tests_for_type! {
 }
 
 tests_for_type! {
+    ExecutionPayloadEnvelope<_>,
+    "consensus-spec-tests/tests/mainnet/heze/ssz_static/ExecutionPayloadEnvelope/*/*",
+    "consensus-spec-tests/tests/minimal/heze/ssz_static/ExecutionPayloadEnvelope/*/*",
+}
+
+tests_for_type! {
+    ExecutionRequests<_>,
+    "consensus-spec-tests/tests/mainnet/heze/ssz_static/ExecutionRequests/*/*",
+    "consensus-spec-tests/tests/minimal/heze/ssz_static/ExecutionRequests/*/*",
+}
+
+tests_for_type! {
+    Fork,
+    "consensus-spec-tests/tests/mainnet/heze/ssz_static/Fork/*/*",
+    "consensus-spec-tests/tests/minimal/heze/ssz_static/Fork/*/*",
+}
+
+tests_for_type! {
+    ForkData,
+    "consensus-spec-tests/tests/mainnet/heze/ssz_static/ForkData/*/*",
+    "consensus-spec-tests/tests/minimal/heze/ssz_static/ForkData/*/*",
+}
+
+tests_for_type! {
+    HistoricalSummary,
+    "consensus-spec-tests/tests/mainnet/heze/ssz_static/HistoricalSummary/*/*",
+    "consensus-spec-tests/tests/minimal/heze/ssz_static/HistoricalSummary/*/*",
+}
+
+tests_for_type! {
+    InclusionList<_>,
+    "consensus-spec-tests/tests/mainnet/heze/ssz_static/InclusionList/*/*",
+    "consensus-spec-tests/tests/minimal/heze/ssz_static/InclusionList/*/*",
+}
+
+tests_for_type! {
+    IndexedAttestation<_>,
+    "consensus-spec-tests/tests/mainnet/heze/ssz_static/IndexedAttestation/*/*",
+    "consensus-spec-tests/tests/minimal/heze/ssz_static/IndexedAttestation/*/*",
+}
+
+tests_for_type! {
+    IndexedPayloadAttestation<_>,
+    "consensus-spec-tests/tests/mainnet/heze/ssz_static/IndexedPayloadAttestation/*/*",
+    "consensus-spec-tests/tests/minimal/heze/ssz_static/IndexedPayloadAttestation/*/*",
+}
+
+tests_for_type! {
+    LightClientBootstrap<_>,
+    "consensus-spec-tests/tests/mainnet/heze/ssz_static/LightClientBootstrap/*/*",
+    "consensus-spec-tests/tests/minimal/heze/ssz_static/LightClientBootstrap/*/*",
+}
+
+tests_for_type! {
+    LightClientFinalityUpdate<_>,
+    "consensus-spec-tests/tests/mainnet/heze/ssz_static/LightClientFinalityUpdate/*/*",
+    "consensus-spec-tests/tests/minimal/heze/ssz_static/LightClientFinalityUpdate/*/*",
+}
+
+tests_for_type! {
+    LightClientHeader,
+    "consensus-spec-tests/tests/mainnet/heze/ssz_static/LightClientHeader/*/*",
+    "consensus-spec-tests/tests/minimal/heze/ssz_static/LightClientHeader/*/*",
+}
+
+tests_for_type! {
+    LightClientOptimisticUpdate<_>,
+    "consensus-spec-tests/tests/mainnet/heze/ssz_static/LightClientOptimisticUpdate/*/*",
+    "consensus-spec-tests/tests/minimal/heze/ssz_static/LightClientOptimisticUpdate/*/*",
+}
+
+tests_for_type! {
+    LightClientUpdate<_>,
+    "consensus-spec-tests/tests/mainnet/heze/ssz_static/LightClientUpdate/*/*",
+    "consensus-spec-tests/tests/minimal/heze/ssz_static/LightClientUpdate/*/*",
+}
+
+tests_for_type! {
+    MatrixEntry<_>,
+    "consensus-spec-tests/tests/mainnet/heze/ssz_static/MatrixEntry/*/*",
+    "consensus-spec-tests/tests/minimal/heze/ssz_static/MatrixEntry/*/*",
+}
+
+tests_for_type! {
+    PayloadAttestation<_>,
+    "consensus-spec-tests/tests/mainnet/heze/ssz_static/PayloadAttestation/*/*",
+    "consensus-spec-tests/tests/minimal/heze/ssz_static/PayloadAttestation/*/*",
+}
+
+tests_for_type! {
+    PayloadAttestationData,
+    "consensus-spec-tests/tests/mainnet/heze/ssz_static/PayloadAttestationData/*/*",
+    "consensus-spec-tests/tests/minimal/heze/ssz_static/PayloadAttestationData/*/*",
+}
+
+tests_for_type! {
+    PayloadAttestationMessage,
+    "consensus-spec-tests/tests/mainnet/heze/ssz_static/PayloadAttestationMessage/*/*",
+    "consensus-spec-tests/tests/minimal/heze/ssz_static/PayloadAttestationMessage/*/*",
+}
+
+tests_for_type! {
+    PendingConsolidation,
+    "consensus-spec-tests/tests/mainnet/heze/ssz_static/PendingConsolidation/*/*",
+    "consensus-spec-tests/tests/minimal/heze/ssz_static/PendingConsolidation/*/*",
+}
+
+tests_for_type! {
+    PendingDeposit,
+    "consensus-spec-tests/tests/mainnet/heze/ssz_static/PendingDeposit/*/*",
+    "consensus-spec-tests/tests/minimal/heze/ssz_static/PendingDeposit/*/*",
+}
+
+tests_for_type! {
+    PendingPartialWithdrawal,
+    "consensus-spec-tests/tests/mainnet/heze/ssz_static/PendingPartialWithdrawal/*/*",
+    "consensus-spec-tests/tests/minimal/heze/ssz_static/PendingPartialWithdrawal/*/*",
+}
+
+tests_for_type! {
+    PowBlock,
+    "consensus-spec-tests/tests/mainnet/heze/ssz_static/PowBlock/*/*",
+    "consensus-spec-tests/tests/minimal/heze/ssz_static/PowBlock/*/*",
+}
+
+tests_for_type! {
+    ProposerPreferences,
+    "consensus-spec-tests/tests/mainnet/heze/ssz_static/ProposerPreferences/*/*",
+    "consensus-spec-tests/tests/minimal/heze/ssz_static/ProposerPreferences/*/*",
+}
+
+tests_for_type! {
+    ProposerSlashing,
+    "consensus-spec-tests/tests/mainnet/heze/ssz_static/ProposerSlashing/*/*",
+    "consensus-spec-tests/tests/minimal/heze/ssz_static/ProposerSlashing/*/*",
+}
+
+tests_for_type! {
+    SignedAggregateAndProof<_>,
+    "consensus-spec-tests/tests/mainnet/heze/ssz_static/SignedAggregateAndProof/*/*",
+    "consensus-spec-tests/tests/minimal/heze/ssz_static/SignedAggregateAndProof/*/*",
+}
+
+tests_for_type! {
+    SignedBeaconBlock<_>,
+    "consensus-spec-tests/tests/mainnet/heze/ssz_static/SignedBeaconBlock/*/*",
+    "consensus-spec-tests/tests/minimal/heze/ssz_static/SignedBeaconBlock/*/*",
+}
+
+tests_for_type! {
+    SignedBeaconBlockHeader,
+    "consensus-spec-tests/tests/mainnet/heze/ssz_static/SignedBeaconBlockHeader/*/*",
+    "consensus-spec-tests/tests/minimal/heze/ssz_static/SignedBeaconBlockHeader/*/*",
+}
+
+tests_for_type! {
+    SignedBlsToExecutionChange,
+    "consensus-spec-tests/tests/mainnet/heze/ssz_static/SignedBLSToExecutionChange/*/*",
+    "consensus-spec-tests/tests/minimal/heze/ssz_static/SignedBLSToExecutionChange/*/*",
+}
+
+tests_for_type! {
+    SignedContributionAndProof<_>,
+    "consensus-spec-tests/tests/mainnet/heze/ssz_static/SignedContributionAndProof/*/*",
+    "consensus-spec-tests/tests/minimal/heze/ssz_static/SignedContributionAndProof/*/*",
+}
+
+tests_for_type! {
     SignedExecutionPayloadBid<_>,
     "consensus-spec-tests/tests/mainnet/heze/ssz_static/SignedExecutionPayloadBid/*/*",
     "consensus-spec-tests/tests/minimal/heze/ssz_static/SignedExecutionPayloadBid/*/*",
 }
 
 tests_for_type! {
-    BeaconState<_>,
-    "consensus-spec-tests/tests/mainnet/heze/ssz_static/BeaconState/*/*",
-    "consensus-spec-tests/tests/minimal/heze/ssz_static/BeaconState/*/*",
+    SignedExecutionPayloadEnvelope<_>,
+    "consensus-spec-tests/tests/mainnet/heze/ssz_static/SignedExecutionPayloadEnvelope/*/*",
+    "consensus-spec-tests/tests/minimal/heze/ssz_static/SignedExecutionPayloadEnvelope/*/*",
+}
+
+tests_for_type! {
+    SignedInclusionList<_>,
+    "consensus-spec-tests/tests/mainnet/heze/ssz_static/SignedInclusionList/*/*",
+    "consensus-spec-tests/tests/minimal/heze/ssz_static/SignedInclusionList/*/*",
+}
+
+tests_for_type! {
+    SignedProposerPreferences,
+    "consensus-spec-tests/tests/mainnet/heze/ssz_static/SignedProposerPreferences/*/*",
+    "consensus-spec-tests/tests/minimal/heze/ssz_static/SignedProposerPreferences/*/*",
+}
+
+tests_for_type! {
+    SignedVoluntaryExit,
+    "consensus-spec-tests/tests/mainnet/heze/ssz_static/SignedVoluntaryExit/*/*",
+    "consensus-spec-tests/tests/minimal/heze/ssz_static/SignedVoluntaryExit/*/*",
+}
+
+tests_for_type! {
+    SigningData,
+    "consensus-spec-tests/tests/mainnet/heze/ssz_static/SigningData/*/*",
+    "consensus-spec-tests/tests/minimal/heze/ssz_static/SigningData/*/*",
+}
+
+tests_for_type! {
+    SingleAttestation,
+    "consensus-spec-tests/tests/mainnet/heze/ssz_static/SingleAttestation/*/*",
+    "consensus-spec-tests/tests/minimal/heze/ssz_static/SingleAttestation/*/*",
+}
+
+tests_for_type! {
+    SyncAggregate<_>,
+    "consensus-spec-tests/tests/mainnet/heze/ssz_static/SyncAggregate/*/*",
+    "consensus-spec-tests/tests/minimal/heze/ssz_static/SyncAggregate/*/*",
+}
+
+tests_for_type! {
+    SyncAggregatorSelectionData,
+    "consensus-spec-tests/tests/mainnet/heze/ssz_static/SyncAggregatorSelectionData/*/*",
+    "consensus-spec-tests/tests/minimal/heze/ssz_static/SyncAggregatorSelectionData/*/*",
+}
+
+tests_for_type! {
+    SyncCommittee<_>,
+    "consensus-spec-tests/tests/mainnet/heze/ssz_static/SyncCommittee/*/*",
+    "consensus-spec-tests/tests/minimal/heze/ssz_static/SyncCommittee/*/*",
+}
+
+tests_for_type! {
+    SyncCommitteeContribution<_>,
+    "consensus-spec-tests/tests/mainnet/heze/ssz_static/SyncCommitteeContribution/*/*",
+    "consensus-spec-tests/tests/minimal/heze/ssz_static/SyncCommitteeContribution/*/*",
+}
+
+tests_for_type! {
+    SyncCommitteeMessage,
+    "consensus-spec-tests/tests/mainnet/heze/ssz_static/SyncCommitteeMessage/*/*",
+    "consensus-spec-tests/tests/minimal/heze/ssz_static/SyncCommitteeMessage/*/*",
+}
+
+tests_for_type! {
+    Validator,
+    "consensus-spec-tests/tests/mainnet/heze/ssz_static/Validator/*/*",
+    "consensus-spec-tests/tests/minimal/heze/ssz_static/Validator/*/*",
+}
+
+tests_for_type! {
+    VoluntaryExit,
+    "consensus-spec-tests/tests/mainnet/heze/ssz_static/VoluntaryExit/*/*",
+    "consensus-spec-tests/tests/minimal/heze/ssz_static/VoluntaryExit/*/*",
+}
+
+tests_for_type! {
+    Withdrawal,
+    "consensus-spec-tests/tests/mainnet/heze/ssz_static/Withdrawal/*/*",
+    "consensus-spec-tests/tests/minimal/heze/ssz_static/Withdrawal/*/*",
+}
+
+tests_for_type! {
+    WithdrawalRequest,
+    "consensus-spec-tests/tests/mainnet/heze/ssz_static/WithdrawalRequest/*/*",
+    "consensus-spec-tests/tests/minimal/heze/ssz_static/WithdrawalRequest/*/*",
 }
